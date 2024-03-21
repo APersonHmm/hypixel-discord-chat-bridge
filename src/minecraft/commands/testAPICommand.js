761a6c45955642d762d3d4e6e1ad4432ce2cbaad
X0RPR0 <nicomittendorf@gmail.com>
2024-03-21 13:35:36 +0100

    h

--- src/minecraft/commands/testAPICommand.js
+++ src/minecraft/commands/testAPICommand.js
@@ -1,38 +1,53 @@
-async onCommand(username, message) {
-    try {
-        const guildData = await fetchGuildAPI();
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+const { fetchPlayerAPI, fetchGuildAPI } = require("../../../API/functions/GuildAPI");
+const fs = require("fs");
 
-        const membersData = await Promise.all(guildData.members.map(async member => {
-            const playerData = await fetchPlayerAPI(member.uuid);
-            return {
-                guildMember: {
-                    uuid: member.uuid,
-                    rank: member.rank,
-                    joined: member.joined,
-                    expHistory: member.expHistory
-                },
-                playerData: {
-                    id: playerData._id,
-                    uuid: playerData.uuid,
-                    displayname: playerData.displayname,
-                    firstLogin: playerData.firstLogin,
-                    lastLogin: playerData.lastLogin
+class TestAPICommand extends minecraftCommand {
+    constructor(minecraft) {
+        super(minecraft);
+
+        this.name = "testapi";
+        this.description = "Fetches data from the GuildAPI and writes the response to a file.";
+    }
+
+    async onCommand(username, message) {
+        try {
+            const guildData = await fetchGuildAPI();
+
+            const membersData = await Promise.all(guildData.members.map(async member => {
+                const playerData = await fetchPlayerAPI(member.uuid);
+                return {
+                    guildMember: {
+                        uuid: member.uuid,
+                        rank: member.rank,
+                        joined: member.joined,
+                        expHistory: member.expHistory
+                    },
+                    playerData: {
+                        id: playerData._id,
+                        uuid: playerData.uuid,
+                        displayname: playerData.displayname,
+                        firstLogin: playerData.firstLogin,
+                        lastLogin: playerData.lastLogin
+                    }
+                };
+            }));
+
+            const output = {
+                guild: {
+                    id: guildData._id,
+                    name: guildData.name,
+                    members: membersData
                 }
             };
-        }));
 
-        const output = {
-            guild: {
-                id: guildData._id,
-                name: guildData.name,
-                members: membersData
-            }
-        };
-
-        fs.writeFileSync('./apiOutput.json', JSON.stringify(output, null, 2));
-        await this.send("API data has been written to apiOutput.json.");
-    } catch (error) {
-        console.error("Error fetching API data:", error);
-        await this.send("An error occurred while fetching API data.");
+            fs.writeFileSync('./apiOutput.json', JSON.stringify(output, null, 2));
+            await this.send("API data has been written to apiOutput.json.");
+        } catch (error) {
+            console.error("Error fetching API data:", error);
+            await this.send("An error occurred while fetching API data.");
+        }
     }
 }
+
+module.exports = TestAPICommand;
