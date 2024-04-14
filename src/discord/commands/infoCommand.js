6a53d39434c72a78d09ff4080a41690d027f200f
X0RPR0 <nicomittendorf@gmail.com>
2024-04-14 22:34:15 +0200

    test

--- src/discord/commands/infoCommand.js
+++ src/discord/commands/infoCommand.js
@@ -1,7 +1,19 @@
 const HypixelDiscordChatBridgeError = require("../../contracts/errorHandler.js");
-const { EmbedBuilder } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 const config = require("../../../config.json");
 const fs = require("fs");
+
+function splitIntoChunks(str, chunkSize) {
+  const chunks = [];
+  let startIndex = 0;
+
+  while (startIndex < str.length) {
+    chunks.push(str.substr(startIndex, chunkSize));
+    startIndex += chunkSize;
+  }
+
+  return chunks;
+}
 
 module.exports = {
   name: "info",
@@ -17,54 +29,55 @@ module.exports = {
 
     const { discordCommands, minecraftCommands } = getCommands(commands);
 
-    const infoEmbed = new EmbedBuilder()
+    const chunkSize = 1024; // Adjust this value based on your needs
+
+    const discordChunks = splitIntoChunks(discordCommands, chunkSize);
+    const minecraftChunks = splitIntoChunks(minecraftCommands, chunkSize);
+
+    const infoEmbed = new MessageEmbed()
       .setColor(0x0099ff)
-      .setTitle("Hypixel Bridge Bot Commands")
-      .addFields(
-        {
-          name: "**Minecraft Commands**: ",
-          value: `${minecraftCommands}`,
-          inline: true,
-        },
-        {
-          name: "**Discord Commands**: ",
-          value: `${discordCommands}`,
-          inline: true,
-        },
-        { name: "\u200B", value: "\u200B" },
-        {
-          name: "**Minecraft Information**:",
-          value: `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
-            config.minecraft.skyblockEventsNotifications.enabled ? "enabled" : "disabled"
-          }\`\nAuto Accept: \`${
-            config.minecraft.guildRequirements.autoAccept ? "enabled" : "disabled"
-          }\`\nGuild Experience Requirement: \`${config.minecraft.guild.guildExp.toLocaleString()}\`\nUptime: Online since <t:${Math.floor(
-            (Date.now() - client.uptime) / 1000
-          )}:R>\nVersion: \`${require("../../../package.json").version}\`\n`,
-          inline: true,
-        },
-        {
-          name: `**Discord Information**`,
-          value: `Guild Channel: ${
-            config.discord.channels.guildChatChannel ? `<#${config.discord.channels.guildChatChannel}>` : "None"
-          }\nOfficer Channel: ${
-            config.discord.channels.officerChannel ? `<#${config.discord.channels.officerChannel}>` : "None"
-          }\nGuild Logs Channel: ${
-            config.discord.channels.loggingChannel ? `<#${config.discord.channels.loggingChannel}>` : "None"
-          }\nDebugging Channel: ${
-            config.discord.channels.debugChannel ? `<#${config.discord.channels.debugChannel}>` : "None"
-          }\nCommand Role: <@&${config.discord.commands.commandRole}>\nMessage Mode: \`${
-            config.discord.other.messageMode
-          }\`\nFilter: \`${config.discord.other.filterMessages ? "enabled" : "disabled"}\`\nJoin Messages: \`${
-            config.discord.other.joinMessage ? "enabled" : "disabled"
-          }\``,
-          inline: true,
-        }
+      .setTitle("Hypixel Bridge Bot Commands");
+
+    discordChunks.forEach((chunk, index) => {
+      infoEmbed.addField(`Discord Commands ${index + 1}`, chunk, true);
+    });
+
+    minecraftChunks.forEach((chunk, index) => {
+      infoEmbed.addField(`Minecraft Commands ${index + 1}`, chunk, true);
+    });
+
+    infoEmbed
+      .addField("\u200B", "\u200B")
+      .addField(
+        "Minecraft Information",
+        `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
+          config.minecraft.skyblockEventsNotifications.enabled ? "enabled" : "disabled"
+        }\`\nAuto Accept: \`${
+          config.minecraft.guildRequirements.autoAccept ? "enabled" : "disabled"
+        }\`\nGuild Experience Requirement: \`${config.minecraft.guild.guildExp.toLocaleString()}\`\nUptime: Online since <t:${Math.floor(
+          (Date.now() - client.uptime) / 1000
+        )}:R>\nVersion: \`${require("../../../package.json").version}\`\n`,
+        true
       )
-      .setFooter({
-        text: "/help [command] for more information",
-        iconURL: "https://imgur.com/tgwQJTX.png",
-      });
+      .addField(
+        "Discord Information",
+        `Guild Channel: ${
+          config.discord.channels.guildChatChannel ? `<#${config.discord.channels.guildChatChannel}>` : "None"
+        }\nOfficer Channel: ${
+          config.discord.channels.officerChannel ? `<#${config.discord.channels.officerChannel}>` : "None"
+        }\nGuild Logs Channel: ${
+          config.discord.channels.loggingChannel ? `<#${config.discord.channels.loggingChannel}>` : "None"
+        }\nDebugging Channel: ${
+          config.discord.channels.debugChannel ? `<#${config.discord.channels.debugChannel}>` : "None"
+        }\nCommand Role: <@&${config.discord.commands.commandRole}>\nMessage Mode: \`${
+          config.discord.other.messageMode
+        }\`\nFilter: \`${config.discord.other.filterMessages ? "enabled" : "disabled"}\`\nJoin Messages: \`${
+          config.discord.other.joinMessage ? "enabled" : "disabled"
+        }\``,
+        true
+      )
+      .setFooter("help [command] for more information", "https://imgur.com/tgwQJTX.png");
+
     await interaction.followUp({ embeds: [infoEmbed] });
   },
 };
