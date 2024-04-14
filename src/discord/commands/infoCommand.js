1e91506da57bcfbac042676679529ae2e12d5149
X0RPR0 <nicomittendorf@gmail.com>
2024-04-14 22:21:48 +0200

    ??

--- src/discord/commands/infoCommand.js
+++ src/discord/commands/infoCommand.js
@@ -3,27 +3,6 @@ const { EmbedBuilder } = require("discord.js");
 const config = require("../../../config.json");
 const fs = require("fs");
 
-function splitFields(text, maxLength = 1024, separator = '\n') {
-    const parts = text.split(separator);
-    const fields = [];
-    let currentField = '';
-
-    for (const part of parts) {
-        if ((currentField + separator + part).length <= maxLength) {
-            currentField += separator + part;
-        } else {
-            fields.push(currentField);
-            currentField = part;
-        }
-    }
-
-    if (currentField) {
-        fields.push(currentField);
-    }
-
-    return fields;
-}
-
 module.exports = {
   name: "info",
   description: "Shows information about the bot.",
@@ -42,52 +21,55 @@ module.exports = {
       .setColor(0x0099ff)
       .setTitle("Hypixel Bridge Bot Commands");
 
-    const minecraftCommandsFields = splitFields(minecraftCommands.join(''));
-    const discordCommandsFields = splitFields(discordCommands.join(''));
+    // Split the commands into chunks of 1024 characters
+    const discordChunks = splitIntoChunks(discordCommands, 1024);
+    const minecraftChunks = splitIntoChunks(minecraftCommands, 1024);
 
-    minecraftCommandsFields.forEach((field, index) => {
-        infoEmbed.addField(`**Minecraft Commands ${index + 1}**: `, field, true);
+    // Add each chunk as a separate field
+    discordChunks.forEach((chunk, index) => {
+      infoEmbed.addField(`**Discord Commands ${index + 1}**:`, chunk);
     });
 
-    discordCommandsFields.forEach((field, index) => {
-        infoEmbed.addField(`**Discord Commands ${index + 1}**: `, field, true);
+    minecraftChunks.forEach((chunk, index) => {
+      infoEmbed.addField(`**Minecraft Commands ${index + 1}**:`, chunk);
     });
 
+    // Add the rest of the fields
     infoEmbed.addFields(
-        { name: "\u200B", value: "\u200B" },
-        {
-          name: "**Minecraft Information**:",
-          value: `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
-            config.minecraft.skyblockEventsNotifications.enabled ? "enabled" : "disabled"
-          }\`\nAuto Accept: \`${
-            config.minecraft.guildRequirements.autoAccept ? "enabled" : "disabled"
-          }\`\nGuild Experience Requirement: \`${config.minecraft.guild.guildExp.toLocaleString()}\`\nUptime: Online since <t:${Math.floor(
-            (Date.now() - client.uptime) / 1000
-          )}:R>\nVersion: \`${require("../../../package.json").version}\`\n`,
-          inline: true,
-        },
-        {
-          name: `**Discord Information**`,
-          value: `Guild Channel: ${
-            config.discord.channels.guildChatChannel ? `<#${config.discord.channels.guildChatChannel}>` : "None"
-          }\nOfficer Channel: ${
-            config.discord.channels.officerChannel ? `<#${config.discord.channels.officerChannel}>` : "None"
-          }\nGuild Logs Channel: ${
-            config.discord.channels.loggingChannel ? `<#${config.discord.channels.loggingChannel}>` : "None"
-          }\nDebugging Channel: ${
-            config.discord.channels.debugChannel ? `<#${config.discord.channels.debugChannel}>` : "None"
-          }\nCommand Role: <@&${config.discord.commands.commandRole}>\nMessage Mode: \`${
-            config.discord.other.messageMode
-          }\`\nFilter: \`${config.discord.other.filterMessages ? "enabled" : "disabled"}\`\nJoin Messages: \`${
-            config.discord.other.joinMessage ? "enabled" : "disabled"
-          }\``,
-          inline: true,
-        }
-      )
-      .setFooter({
-        text: "/help [command] for more information",
-        iconURL: "https://i.imgur.com/vt9IRtV.png",
-      });
+      { name: "\u200B", value: "\u200B" },
+      {
+        name: "**Minecraft Information**:",
+        value: `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
+          config.minecraft.skyblockEventsNotifications.enabled ? "enabled" : "disabled"
+        }\`\nAuto Accept: \`${
+          config.minecraft.guildRequirements.autoAccept ? "enabled" : "disabled"
+        }\`\nGuild Experience Requirement: \`${config.minecraft.guild.guildExp.toLocaleString()}\`\nUptime: Online since <t:${Math.floor(
+          (Date.now() - client.uptime) / 1000
+        )}:R>\nVersion: \`${require("../../../package.json").version}\`\n`,
+        inline: true,
+      },
+      {
+        name: `**Discord Information**`,
+        value: `Guild Channel: ${
+          config.discord.channels.guildChatChannel ? `<#${config.discord.channels.guildChatChannel}>` : "None"
+        }\nOfficer Channel: ${
+          config.discord.channels.officerChannel ? `<#${config.discord.channels.officerChannel}>` : "None"
+        }\nGuild Logs Channel: ${
+          config.discord.channels.loggingChannel ? `<#${config.discord.channels.loggingChannel}>` : "None"
+        }\nDebugging Channel: ${
+          config.discord.channels.debugChannel ? `<#${config.discord.channels.debugChannel}>` : "None"
+        }\nCommand Role: <@&${config.discord.commands.commandRole}>\nMessage Mode: \`${
+          config.discord.other.messageMode
+        }\`\nFilter: \`${config.discord.other.filterMessages ? "enabled" : "disabled"}\`\nJoin Messages: \`${
+          config.discord.other.joinMessage ? "enabled" : "disabled"
+        }\``,
+        inline: true,
+      }
+    )
+    .setFooter({
+      text: "/help [command] for more information",
+      iconURL: "https://imgur.com/tgwQJTX.png",
+    });
 
     await interaction.followUp({ embeds: [infoEmbed] });
   },
@@ -98,7 +80,8 @@ function getCommands(commands) {
     .map(({ name, options }) => {
       const optionsString = options?.map(({ name, required }) => (required ? ` (${name})` : ` [${name}]`)).join("");
       return `- \`${name}${optionsString ? optionsString : ""}\`\n`;
-    });
+    })
+    .join("");
 
   const minecraftCommands = fs
     .readdirSync("./src/minecraft/commands")
@@ -110,7 +93,20 @@ function getCommands(commands) {
         .join("");
 
       return `- \`${command.name}${optionsString}\`\n`;
-    });
+    })
+    .join("");
 
   return { discordCommands, minecraftCommands };
+}
+
+function splitIntoChunks(text, maxLength) {
+  const chunks = [];
+  let i = 0;
+
+  while (i < text.length) {
+    chunks.push(text.slice(i, i + maxLength));
+    i += maxLength;
+  }
+
+  return chunks;
 }
