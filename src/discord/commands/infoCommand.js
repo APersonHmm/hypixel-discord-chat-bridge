6a12b82d38910b5835c04cd8ab3f0fdd7cf05fcf
X0RPR0 <nicomittendorf@gmail.com>
2024-04-14 22:58:42 +0200

    test ?

--- src/discord/commands/infoCommand.js
+++ src/discord/commands/infoCommand.js
@@ -1,5 +1,5 @@
 const HypixelDiscordChatBridgeError = require("../../contracts/errorHandler.js");
-const { EmbedBuilder } = require("@discordjs/builders");
+const { EmbedBuilder } = require("discord.js");
 const config = require("../../../config.json");
 const fs = require("fs");
 
@@ -17,45 +17,54 @@ module.exports = {
 
     const { discordCommands, minecraftCommands } = getCommands(commands);
 
-    const discordChunks = chunkArray(discordCommands, 10);
-    const minecraftChunks = chunkArray(minecraftCommands, 10);
+    const chunkSize = 10; // Adjust this value based on your needs
+    const discordChunks = [];
+    const minecraftChunks = [];
+
+    for (let i = 0; i < discordCommands.length; i += chunkSize) {
+      discordChunks.push(discordCommands.slice(i, i + chunkSize));
+    }
+
+    for (let i = 0; i < minecraftCommands.length; i += chunkSize) {
+      minecraftChunks.push(minecraftCommands.slice(i, i + chunkSize));
+    }
 
     const infoEmbed = new EmbedBuilder()
       .setColor(0x0099ff)
       .setTitle("Hypixel Bridge Bot Commands");
 
-      discordChunks.forEach((chunk, index) => {
-        infoEmbed.addFields({
-          name: `Discord Commands ${index + 1}`,
-          value: chunk.join('\n'),
-          inline: true,
-        });
+    discordChunks.forEach((chunk, index) => {
+      infoEmbed.addFields({
+        name: `Discord Commands ${index + 1}`,
+        value: chunk.join('\n'),
+        inline: true,
       });
+    });
 
-      minecraftChunks.forEach((chunk, index) => {
-        infoEmbed.addFields({
-          name: `Minecraft Commands ${index + 1}`,
-          value: chunk.join('\n'),
-          inline: true,
-        });
+    minecraftChunks.forEach((chunk, index) => {
+      infoEmbed.addFields({
+        name: `Minecraft Commands ${index + 1}`,
+        value: chunk.join('\n'),
+        inline: true,
       });
+    });
 
-    infoEmbed
-      .addFields("\u200B", "\u200B")
-      .addFields(
-        "Minecraft Information",
-        `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
+    infoEmbed.addFields(
+      { name: "\u200B", value: "\u200B" },
+      {
+        name: "**Minecraft Information**:",
+        value: `Bot Username: \`${bot.username}\`\nPrefix: \`${config.minecraft.bot.prefix}\`\nSkyBlock Events: \`${
           config.minecraft.skyblockEventsNotifications.enabled ? "enabled" : "disabled"
         }\`\nAuto Accept: \`${
           config.minecraft.guildRequirements.autoAccept ? "enabled" : "disabled"
         }\`\nGuild Experience Requirement: \`${config.minecraft.guild.guildExp.toLocaleString()}\`\nUptime: Online since <t:${Math.floor(
           (Date.now() - client.uptime) / 1000
         )}:R>\nVersion: \`${require("../../../package.json").version}\`\n`,
-        true
-      )
-      .addFields(
-        "Discord Information",
-        `Guild Channel: ${
+        inline: true,
+      },
+      {
+        name: `**Discord Information**`,
+        value: `Guild Channel: ${
           config.discord.channels.guildChatChannel ? `<#${config.discord.channels.guildChatChannel}>` : "None"
         }\nOfficer Channel: ${
           config.discord.channels.officerChannel ? `<#${config.discord.channels.officerChannel}>` : "None"
@@ -68,9 +77,13 @@ module.exports = {
         }\`\nFilter: \`${config.discord.other.filterMessages ? "enabled" : "disabled"}\`\nJoin Messages: \`${
           config.discord.other.joinMessage ? "enabled" : "disabled"
         }\``,
-        true
-      )
-      .setFooter("help [command] for more information", "https://imgur.com/tgwQJTX.png");
+        inline: true,
+      }
+    )
+    .setFooter({
+      text: "/help [command] for more information",
+      iconURL: "https://imgur.com/tgwQJTX.png",
+    });
 
     await interaction.followUp({ embeds: [infoEmbed] });
   },
@@ -96,17 +109,4 @@ function getCommands(commands) {
     });
 
   return { discordCommands, minecraftCommands };
-}
-
-function chunkArray(myArray, chunk_size){
-    var index = 0;
-    var arrayLength = myArray.length;
-    var tempArray = [];
-    
-    for (index = 0; index < arrayLength; index += chunk_size) {
-        myChunk = myArray.slice(index, index+chunk_size);
-        tempArray.push(myChunk);
-    }
-
-    return tempArray;
 }
