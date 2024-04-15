8b2f9d8b0700f92367d54ce2e5d74933a1f0af33
X0RPR0 <nicomittendorf@gmail.com>
2024-04-15 02:28:20 +0200

    hopefully changed layout

--- src/discord/commands/calenderCommand.js
+++ src/discord/commands/calenderCommand.js
@@ -21,11 +21,34 @@ module.exports = {
 
     console.log("Building embed message...");
 
-    for (const event in EVENTS.data.events) {
-      const eventData = EVENTS.data.events[event];
-      let dates = eventData.events.map(e => new Date(e.start_timestamp).toLocaleString()).slice(0, 5);
-      embed.addFields({ name: eventData.name, value: dates.join('\n'), inline: true });
-    }
+    // ELECTION
+    embed.addFields(
+      { name: 'Election Opens', value: `<t:${Math.floor(EVENTS.data.events.ELECTION_BOOTH_OPENS.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Election Closes', value: `<t:${Math.floor(EVENTS.data.events.ELECTION_OVER.events[0].start_timestamp / 1000)}:R>`, inline: true },
+    );
+
+    // BANK
+    embed.addFields(
+      { name: 'Bank Interest', value: `<t:${Math.floor(EVENTS.data.events.BANK_INTEREST.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Dark Auction', value: `<t:${Math.floor(EVENTS.data.events.DARK_AUCTION.events[0].start_timestamp / 1000)}:R>`, inline: true },
+    );
+
+    // EVENTS
+    embed.addFields(
+      { name: 'Spooky Festival', value: `<t:${Math.floor(EVENTS.data.events.SPOOKY_FESTIVAL.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Fear Mongerer', value: `<t:${Math.floor(EVENTS.data.events.FEAR_MONGERER.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Jacob Contest', value: EVENTS.data.events.JACOBS_CONTEST.events.slice(0, 3).map(e => `<t:${Math.floor(e.start_timestamp / 1000)}:R>`).join('\n'), inline: true },
+      { name: 'Fallen Star Cult', value: EVENTS.data.events.FALLEN_STAR_CULT.events.slice(0, 3).map(e => `<t:${Math.floor(e.start_timestamp / 1000)}:R>`).join('\n'), inline: true },
+      { name: 'Traveling Zoo', value: `<t:${Math.floor(EVENTS.data.events.TRAVELING_ZOO.events[0].start_timestamp / 1000)}:R>`, inline: true },
+    );
+
+    // JERRY
+    embed.addFields(
+      { name: 'Jerrys Workshop opens', value: `<t:${Math.floor(EVENTS.data.events.JERRYS_WORKSHOP.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Season of Jerry', value: `<t:${Math.floor(EVENTS.data.events.SEASON_OF_JERRY.events[0].start_timestamp / 1000)}:R>`, inline: true },
+      { name: 'Jerrys Workshop closes', value: `<t:${Math.floor(EVENTS.data.events.JERRYS_WORKSHOP.events[0].end_timestamp / 1000)}:R>`, inline: true },
+      { name: 'New Year Celebration (Cakes)', value: `<t:${Math.floor(EVENTS.data.events.NEW_YEAR_CELEBRATION.events[0].start_timestamp / 1000)}:R>`, inline: true },
+    );
 
     console.log("Embed message built:", embed);
 
